acbe71a9102188fd23519e70293976827d1709ff
pastuchenko <pablopastuchenkooo@gmail.com>
2024-10-03 08:55:27 -0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// Substitua module.exports por export default
+
+const nextConfig = {
+  images: {
+    domains: ['cdn.imagin.studio'], // Defina os domínios permitidos aqui
+  },
+}
+
+export default nextConfig;
